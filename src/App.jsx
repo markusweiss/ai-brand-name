5143c833c621b9d5cc08bffb0fdedfed8561364d
markusweiss <git@code71.de>
2023-04-18 15:00:28 +0200

    testing static vercel url

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,7 @@ const App = () => {
   }
 
   try {
-    const response = await fetch('http://localhost:8000/completions', options);
+    const response = await fetch('https://ai-brand-name.vercel.app:5000/completions', options);
     const data = await response.json();
     //console.log("DATA: ", data);
     setMessage(data.choices[0].message.content);
